6facbc9386bb40151831719da7b7d99bb12d7ee9
otinomz <awesomeotinomz@gmail.com>
2022-01-05 20:38:40 +0100

    setting up the router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import CoinPage from './pages/CoinPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      
-      <h1>Crypto Price Tracker With React js</h1>
-      
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        
+        <Route path="/" component={HomePage }/>
+        <Route path="/coins/:id" component={CoinPage}/>
+        
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
