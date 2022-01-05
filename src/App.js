ffec7366557178c49227d6179b272e1f1bfc055d
otinomz <awesomeotinomz@gmail.com>
2022-01-05 20:39:42 +0100

    .

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div>
         <Header />
         
-        <Route path="/" component={HomePage }/>
+        <Route path="/" component={HomePage } exact/>
         <Route path="/coins/:id" component={CoinPage}/>
         
       </div>
